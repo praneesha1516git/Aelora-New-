921c39a04b6053354e7ace4580ebdaec5ba4121b
praneesha_sliit <praneesha2003perera@gmail.com>
2025-12-12 20:34:38 +0530

    Implement CreateSolarUnitForm component and integrate it into the solar unit creation page

--- steml-link-frontend/src/pages/admin/solar-unit-create.page.jsx
+++ steml-link-frontend/src/pages/admin/solar-unit-create.page.jsx
@@ -1,16 +1,7 @@
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { CreateSolarUnitForm } from "./components/CreateSolarUnitForm";
 
 
 export default function SolarUnitCreatePage() {
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const data = Object.fromEntries(formData);
-        console.log("Create solar unit with data: ", data);
 
 
     return (
@@ -18,20 +9,8 @@ export default function SolarUnitCreatePage() {
       <h1 className="text-4xl font-bold text-foreground">Create Solar Unit</h1>
       <p className="text-gray-600 mt-2">Create a new solar unit</p>
       <div className="mt-8">
-        <form onSubmit={handleSubmit}>
-          <div className="w-1/2">
-            <div className="grid gap-2">
-              <Label htmlFor="serialNumber">Serial Number</Label>
-              <Input type="text" id="serialNumber" required name="serialNumber" />
-            </div>
-
-            <div className="mt-4">
-            <Button type="submit">Create</Button> 
-            </div>
-          </div>
-        </form>
+        <CreateSolarUnitForm />
       </div>
     </main>
     );
-}
 }
